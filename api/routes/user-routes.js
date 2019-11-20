3331043c462657a5166184330239b06fa149b390
Nolan Allen <nolan.r.allen@gmail.com>
2019-11-20 14:53:28 -0600

    Reformatted findById route into async function; works

--- api/routes/user-routes.js
+++ api/routes/user-routes.js
@@ -14,23 +14,34 @@ router.get('/users', restricted, (req, res) => {
     });
 });
 
-router.get('/users/:id', restricted, (req, res) => {
+router.get('/users/:id', restricted, async (req, res) => {
   const { id } = req.params;
   
-  Users
-    .findById(id)
-    .then(users => {
-      const user = users[0];
+  try {
+    const user = await Users.findById(id);
+    if(user.id) {
+      res.status(200).json(user);
+    } else if(!user.id) {
+      res.status(404).json({ message: `User could not be found.` });
+    };
+  } catch(err) {
+    res.status(500).json(err);
+  };
 
-      if(user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: 'User not found.' });
-      };
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+  // Users
+  //   .findById(id)
+  //   .then(users => {
+  //     const user = users[0];
+
+  //     if(user) {
+  //       res.status(200).json(user);
+  //     } else {
+  //       res.status(404).json({ message: 'User not found.' });
+  //     };
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json(err);
+  //   });
 });
 
 module.exports = router;
