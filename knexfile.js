6fd4ce4285c225d450711ac92c3f4d45f0cecc86
Nolan Allen <nolan.r.allen@gmail.com>
2019-11-19 18:00:53 -0600

    7th commit: modified knexfile to fix deployment bug

--- knexfile.js
+++ knexfile.js
@@ -39,36 +39,19 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/nutrition_tracker.db3'
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
     },
-    useNullAsDefault: true,
     migrations: {
+      tableName: 'knex_migrations',
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
-    },
-
-    // client: 'pg',
-    // connection: process.env.DATABASE_URL + '?ssl=true',
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   tableName: 'knex_migrations',
-    //   directory: './data/migrations'
-    // },
-    // seeds: {
-    //   directory: './data/seeds'
-    // }
+    }
   },
 
 };
