5701a178be2a1be225ead1a53eb25065d9b9cd02
Nolan Allen <nolan.r.allen@gmail.com>
2019-11-23 03:21:55 -0600

    Removed the 'roles' and 'users_roles' table for testing

--- data/migrations/20191118150201_permissions.js
+++ data/migrations/20191118150201_permissions.js
@@ -27,36 +27,36 @@ exports.up = function(knex) {
         .notNullable();
 
     })
-    .createTable('roles', roles => {
-      roles.increments();
-      roles
-        .string('title', 255)
-        .notNullable();
-    })
-    .createTable('users_roles', tbl => {
-      tbl.increments();
-      tbl
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('NO ACTION')
-        .onUpdate('NO ACTION')
-        .notNullable();
-      tbl
-        .integer('role_id')
-        .unsigned()
-        .references('id')
-        .inTable('roles')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-        .notNullable();
-    });
+    // .createTable('roles', roles => {
+    //   roles.increments();
+    //   roles
+    //     .string('title', 255)
+    //     .notNullable();
+    // })
+    // .createTable('users_roles', tbl => {
+    //   tbl.increments();
+    //   tbl
+    //     .integer('user_id')
+    //     .unsigned()
+    //     .references('id')
+    //     .inTable('users')
+    //     .onDelete('NO ACTION')
+    //     .onUpdate('NO ACTION')
+    //     .notNullable();
+    //   tbl
+    //     .integer('role_id')
+    //     .unsigned()
+    //     .references('id')
+    //     .inTable('roles')
+    //     .onDelete('CASCADE')
+    //     .onUpdate('CASCADE')
+    //     .notNullable();
+    // });
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users_roles')
-    .dropTableIfExists('roles')
+    // .dropTableIfExists('users_roles')
+    // .dropTableIfExists('roles')
     .dropTableIfExists('users');
 };
